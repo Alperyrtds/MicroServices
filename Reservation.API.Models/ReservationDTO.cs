c0955ad35f66f41f2caf84df4a83e2e6334dc1d3
Alper Yurtdaş <73263073+Alperyrtds@users.noreply.github.com>
2021-08-02 15:22:40 +0300

    Add project files.

---
+++ Reservation.API.Models/ReservationDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reservation.API.Models
+{
+    public class ReservationDTO
+    {
+        public int Id { get; set; }
+        public int BkgNumber { get; set; }
+        public DateTime CheckinDate { get; set; }
+        public DateTime CheckOutDate { get; set; }
+        public DateTime BkgDate { get; set; }
+        public decimal Amount { get; set; }
+        
+    }
+}
---
+++ Services/ResAPI/Reservation.API/Controllers/ResController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Reservation.API.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reservation.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ResController : ControllerBase
+    {
+        private readonly IReservationService _reservationService;
+        public ResController(IReservationService reservationService)
+        {
+            _reservationService = reservationService;
+        }
+
+        [HttpGet("{id}")]
+        public Reservation.API.Models.ReservationDTO Get(int id)
+        {
+            return _reservationService.GetResByBkgNumber(id);
+        }
+    }
+}
